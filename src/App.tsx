62423fbbcf29b82180f6a0eead03e610662b76ff
StormyData <30272275+StormyData@users.noreply.github.com>
2024-12-03 10:23:30 +0100

    add toast if device is not supported

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore } from '@react-three/xr'
 import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const store = createXRStore()
+const store = createXRStore();
 
 export default function App() {
-  const [red, setRed] = useState<boolean>(false)
+  const [red, setRed] = useState<boolean>(false);
+  async function enterSession() {
+    await store.enterAR().catch((reason) => {
+      toast(`your browser does not support this application, reason: ${reason}`);
+    });
+  }
   return (
     <>
-      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <button onClick={enterSession}>Enter AR</button>
       <Canvas>
         <XR store={store}>
           <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
@@ -17,6 +24,7 @@ export default function App() {
           </mesh>
         </XR>
       </Canvas>
+      <ToastContainer/>
     </>
   )
 }
