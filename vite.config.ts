385baa435f3363006284dcffd1fc4e1201e5b8d8
StormyData <30272275+StormyData@users.noreply.github.com>
2025-01-12 12:35:30 +0100

    change build target

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
   return {
+    build: {
+      target: 'esnext'
+    },
     plugins: [react()],
     base: env.VITE_BASE_PATH ?? "/",
     server: {
